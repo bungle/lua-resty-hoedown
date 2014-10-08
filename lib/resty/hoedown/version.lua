724fb808414c95f6942d837174dd465e839d554c
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2014-10-08 19:26:54 +0300

    Added version bindings.

--- lib/resty/hoedown/version.lua
+++ lib/resty/hoedown/version.lua
@@ -1,8 +1,32 @@
---
--- Created by IntelliJ IDEA.
--- User: bungle
--- Date: 7.10.2014
--- Time: 0.43
--- To change this template use File | Settings | File Templates.
---
+local lib      = require "resty.hoedown.library"
+local ffi      = require "ffi"
+local ffi_new  = ffi.new
+local ffi_cdef = ffi.cdef
+local tonumber = tonumber
+local concat   = table.concat
 
+ffi_cdef[[
+void hoedown_version(int *major, int *minor, int *revision);
+]]
+
+local version = {}
+
+version.__index = version
+
+function version:__tostring()
+    return concat({ self.major, self.minor, self.revision }, '.')
+end
+
+do
+    local major    = ffi_new("int[1]", 0)
+    local minor    = ffi_new("int[1]", 0)
+    local revision = ffi_new("int[1]", 0)
+
+    lib.hoedown_version(major, minor, revision)
+
+    version.major    = tonumber(major[0])
+    version.minor    = tonumber(minor[0])
+    version.revision = tonumber(revision[0])
+end
+
+return setmetatable(version, version)
